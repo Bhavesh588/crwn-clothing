e5b12452ee9522bbafd37a289f41a7847213dea2
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-03-19 11:20:09 +0530

    Adding new files

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
-
-const Hatspage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Shoppage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={Hatspage} />
+        <Route path='/shop' component={Shoppage} />
       </Switch>
     </div>
   );
