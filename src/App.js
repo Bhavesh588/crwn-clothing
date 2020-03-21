acba88def2e8c3a7301b53bb77f5309cdd0f7b50
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-03-21 18:01:09 +0530

    Adding items to checkout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -45,7 +45,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={Shoppage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />) } />
         </Switch>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,14 @@ const config = {
     measurementId: "G-GZQRSVC9S9"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
     const userRef  = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = userRef.get();
+    const snapShot = await userRef.get();
 
     if(!snapShot.exists) {
         const { displayName, email } = userAuth;
@@ -39,7 +41,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 }
 
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
