7c0ec2cc4d8745c9fdcaa47342a2dbf1bc719a80
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-03-25 10:38:34 +0530

    Changing Files

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,25 +9,48 @@ import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/fireb
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
-class Shoppage extends React.Component{
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class Shoppage extends React.Component {
+    state = {
+        loading: true
+    };
+
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collection');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get()
+            .then(async snapshot => {
             const collectionMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionMap);
+            this.setState({ loading: false });
         })
     }
 
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverview} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route 
+                    exact 
+                    path={`${match.path}`} 
+                    render={props => (
+                        <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+                    )} 
+                />
+                <Route
+                    path={`${match.path}/:collectionId`} 
+                    render={props => (
+                        <CollectionPageWithSpinner isLoading={loading} {...props} />
+                    )} 
+                />
             </div>
         );
     }
